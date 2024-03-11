d369c52e94ea1765215bd8f080e200dd88f04513
rajharshcr7 <rajharsh.footballer@gmail.com>
2024-03-11 15:30:06 +0530

    completed algo 1 part 1 --multicast tree

--- Working/algorithm/multicast-tree/build_multicast_tree.cpp
+++ Working/algorithm/multicast-tree/build_multicast_tree.cpp
@@ -5,6 +5,26 @@ namespace fs = std::filesystem;
 
 using namespace std;
 
+/**
+ * @brief Pair of {dist, Node}
+ *
+ */
+typedef pair<int, Node *> distNode;
+/**
+ * @brief Pair of {dist, {Node*, {Node *, []Node*}
+ *
+ */
+typedef pair<int, pair<Node *, pair<Node *, vector<distNode>>>> distSrcDestNodesPair;
+/**
+ * @brief Pair of {dist, dist} or {Node, Node}
+ *
+ */
+typedef pair<int, int> dist_node_pair;
+
+/**
+ * @brief TreeInputRequestBlock class handles request inputs.
+ *
+ */
 class TreeInputRequestBlock
 {
 public:
@@ -78,6 +98,11 @@ public:
     }
 };
 
+/**
+ * @brief Collect requests from a given directory
+ *
+ * @return vector<TreeInputRequestBlock>
+ */
 vector<TreeInputRequestBlock> CollectRequests()
 {
     string path = "../../requests";
@@ -123,12 +148,23 @@ vector<TreeInputRequestBlock> CollectRequests()
     return requests;
 }
 
-pair<int, pair<Node *, Node *>> DijkstraAlgo(Node *src, vector<int> dest, unordered_map<int, Node *> ControlBlock, vector<Node *> treeNodes)
+/**
+ * @brief Finds SSSP for a given node.
+ *
+ * @param src Source Node
+ * @param dest Destinations
+ * @param ControlBlock Node Control Block
+ * @param treeNodes Nodes that are added to Steiner tree
+ * @return pair<int, pair<Node *, pair<Node *, vector<distNode>>>>
+ */
+distSrcDestNodesPair DijkstraAlgo(Node *src, vector<int> dest, unordered_map<int, Node *> ControlBlock, vector<Node *> treeNodes)
 {
-    priority_queue<pair<int, Node *>, vector<pair<int, Node *>>, greater<pair<int, Node *>>> keepDestinations;
+    priority_queue<distNode, vector<distNode>, greater<distNode>> keepDestinations;
     vector<int> destinations(ControlBlock.size(), INT_MAX);
+    vector<distNode> parents(ControlBlock.size());
     destinations[src->node - 1] = 0;
     keepDestinations.push({0, src});
+    parents[src->node - 1] = {0, nullptr};
 
     while (!keepDestinations.empty())
     {
@@ -140,54 +176,122 @@ pair<int, pair<Node *, Node *>> DijkstraAlgo(Node *src, vector<int> dest, unorde
             if (v.second + destinations[u->node - 1] < destinations[v.first->node - 1])
             {
                 destinations[v.first->node - 1] = v.second + destinations[u->node - 1];
+                parents[v.first->node - 1] = {destinations[v.first->node - 1], u};
                 keepDestinations.push({destinations[v.first->node - 1], v.first});
             }
         }
     }
 
-    priority_queue<pair<int, Node *>, vector<pair<int, Node *>>, greater<pair<int, Node *>>> shortestDestination;
+    distNode shortestDestination = {INT_MAX, nullptr};
 
     for (auto v : dest)
     {
         if (find(treeNodes.begin(), treeNodes.end(), ControlBlock[v]) == treeNodes.end())
         {
-            shortestDestination.push({destinations[v - 1], ControlBlock[v]});
+            if (destinations[v - 1] < shortestDestination.first)
+            {
+                shortestDestination = {destinations[v - 1], ControlBlock[v]};
+            }
         }
     }
 
-    return {shortestDestination.top().first, {src, shortestDestination.top().second}};
+    // Store path from source to destination i.e. src, internmediate and destination nodes.
+    vector<distNode> path;
+    path.push_back(parents[shortestDestination.second->node - 1]);
+    distNode temp = parents[shortestDestination.second->node - 1];
+    while (temp.second != src)
+    {
+        path.push_back(parents[temp.second->node - 1]);
+        temp = parents[temp.second->node - 1];
+    }
+
+    return {shortestDestination.first, {src, {shortestDestination.second, path}}};
 }
 
+/**
+ * @brief Generates a multicast tree for each request.
+ *
+ * @param ControlBlock
+ * @param req
+ */
 void MulticastTree(unordered_map<int, Node *> ControlBlock, TreeInputRequestBlock req)
 {
     vector<Node *> treeNodes;
-    queue<pair<int, int>> paths;
+    vector<stack<dist_node_pair>> paths(req.dest.size());
+    int servedPath = 0;
 
     // add source to tree
     treeNodes.push_back(ControlBlock[req.src]);
 
-    // for each node in steiner tree find shortest destination from destination set
-    while (treeNodes.size() != req.dest.size() + 1)
+    // For printing purpose, show the user what was the src, dest and demand
+    cout << "Source: " << req.src << " Destinations: ";
+    for (auto v : req.dest)
     {
-        // min heap
-        priority_queue<pair<int, pair<Node *, Node *>>, vector<pair<int, pair<Node *, Node *>>>, greater<pair<int, pair<Node *, Node *>>>> shortestFromTree;
+        cout << v << " ";
+    }
+    cout << "Demand: " << req.demand << endl;
+    // The result is manipulated following...
+
+    // for each node in steiner tree find shortest destination from destination set
+    while (req.dest.size())
+    {
+        // Holds shortest distance from source to destination
+        distSrcDestNodesPair shortestFromTree = {INT_MAX, {nullptr, {nullptr, {}}}};
         for (auto v : treeNodes)
         {
-            shortestFromTree.push(DijkstraAlgo(v, req.dest, ControlBlock, treeNodes));
+            distSrcDestNodesPair algoResult = DijkstraAlgo(v, req.dest, ControlBlock, treeNodes);
+            if (algoResult.first < shortestFromTree.first)
+                shortestFromTree = algoResult;
         }
-        treeNodes.push_back(shortestFromTree.top().second.second);
-        paths.push({shortestFromTree.top().second.first->node, shortestFromTree.top().second.second->node});
+
+        // Add the destination to the steiner tree
+        treeNodes.push_back(shortestFromTree.second.second.first);
+        paths[servedPath].push({shortestFromTree.first, shortestFromTree.second.second.first->node});
+
+        // Add intermediate nodes from source to destination
+        for (auto v : shortestFromTree.second.second.second)
+        {
+            paths[servedPath].push({v.first, v.second->node});
+            if (v.second != nullptr && find(treeNodes.begin(), treeNodes.end(), v.second) == treeNodes.end())
+            {
+                treeNodes.push_back(v.second);
+            }
+        }
+
+        // <--- Start: Print intermediate paths --->
+        // for (auto v : shortestFromTree.second.second.second)
+        // {
+        //     cout << v.first << " ";
+        //     v.second == nullptr ? cout << "null" : cout << v.second->node;
+        //     cout << endl;
+        // }
+        // <--- End: Print intermediate paths --->
+        // paths.push({shortestFromTree.second.first->node, shortestFromTree.second.second.first->node});
+
+        // Remove the destination from "dest" which is added to the steiner tree
+        auto it = find(req.dest.begin(), req.dest.end(), shortestFromTree.second.second.first->node);
+        req.dest.erase(it);
+        servedPath++;
     }
 
-    cout << "Tree: ";
-    while (!paths.empty())
+    cout << "Tree: " << endl;
+    for (auto v : paths)
     {
-        cout << "(" << paths.front().first << " -> " << paths.front().second << ") ";
-        paths.pop();
+        cout << "(";
+        while (!v.empty())
+        {
+            cout << v.top().second;
+            v.size() > 1 ? cout << " -> " : cout << "";
+            v.pop();
+        }
+        cout << ") " << endl;
     }
-    cout << endl;
 }
 
+/**
+ * @brief Algorithm 1
+ *
+ */
 void BuildTree()
 {
     // Step 1: Create adjacency list
