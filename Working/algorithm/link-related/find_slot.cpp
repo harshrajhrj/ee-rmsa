003f9a7f1f9fdef962a6a6e78d1c13c3e4ede612
rajharshcr7 <rajharsh.footballer@gmail.com>
2024-03-30 22:53:14 +0530

    created findSlots file

--- Working/algorithm/link-related/find_slot.cpp
+++ Working/algorithm/link-related/find_slot.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int FindSlots() {
-    return;
+void FindSlots()
+{
+    vector<int> v{4, 5, 6, 6, 444};
+
+    cout << *max_element(v.begin(), v.end());
+}
+
+int main() {
+    FindSlots();
 }
